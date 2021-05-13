b5f13dffc8c90c27285b26cc11b4a8089f637658
Fabyam <fabyamsalazar@gmail.com>
2021-05-13 18:00:29 -0400

    add list frag and display it

---
+++ guide_list/src/main/java/com/fabyloso/guide_list/GuideListFragment.kt
@@ -0,0 +1,31 @@
+package com.fabyloso.guide_list
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GuideListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = GuideListFragment()
+    }
+
+    private lateinit var viewModel: GuideListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.guide_list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(GuideListViewModel::class.java)
+
+    }
+
+}
---
+++ guide_list/src/main/java/com/fabyloso/guide_list/GuideListViewModel.kt
@@ -0,0 +1,7 @@
+package com.fabyloso.guide_list
+
+import androidx.lifecycle.ViewModel
+
+class GuideListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
